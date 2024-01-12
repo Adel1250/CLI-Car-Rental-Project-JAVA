81435e0f5e17551363783f6d49f561addf35fb54
Adel1250 <adelashrafhamouda@gmail.com>
2024-01-12 20:01:06 +0200

    Insert and read from bookings.csv

--- src/com/adel/booking/BookingArrayDataAccessService.java
+++ src/com/adel/booking/BookingArrayDataAccessService.java
@@ -1,5 +1,8 @@
 package com.adel.booking;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class BookingArrayDataAccessService implements BookingDao {
     private static final Booking[] bookings;
     private static int bookingsCount;
@@ -7,11 +10,13 @@ public class BookingArrayDataAccessService implements BookingDao {
     static {
         bookingsCount = 0;
         bookings = new Booking[50];
+        readCSVFile();
     }
 
     @Override
     public void insertBooking(Booking booking) {
         bookings[bookingsCount] = booking;
+        insertIntoCSVFile(booking);
         bookingsCount++;
         if (bookingsCount == 50) {
             bookingsCount = 0;
@@ -44,5 +49,57 @@ public class BookingArrayDataAccessService implements BookingDao {
             }
         }
         return null;
+    }
+
+    private static void readCSVFile() {
+        File file = new File("src/com/adel/bookings.csv");
+        try {
+            if (!file.exists()) {
+                if (file.createNewFile()) {
+                    System.out.println("Bookings file is created successfully!");
+                }
+            }
+        } catch (java.io.IOException e) {
+            System.out.println("Can't create the file!");
+        }
+        Scanner scanner;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        while (scanner.hasNext()) {
+            String[] bookingLineValues = scanner.nextLine().split(", ");
+            Booking booking = new Booking(bookingLineValues[0], bookingLineValues[1]);
+            bookings[bookingsCount] = booking;
+            bookingsCount++;
+            if (bookingsCount == 50) {
+                bookingsCount = 0;
+            }
+        }
+    }
+
+    private static void insertIntoCSVFile(Booking booking) {
+        File file = new File("src/com/adel/bookings.csv");
+        try {
+            if (!file.exists()) {
+                if (file.createNewFile()) {
+                    System.out.println("Bookings file is created successfully!");
+                }
+            }
+        } catch (java.io.IOException e) {
+            System.out.println("Can't create the file!");
+        }
+        FileWriter fileWriter;
+        try {
+            fileWriter = new FileWriter(file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        printWriter.println(booking.getUserUUID() + ", " + booking.getCarID());
+
+        printWriter.flush();
+        printWriter.close();
     }
 }
